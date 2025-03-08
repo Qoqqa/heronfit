aa2d6e0424146d4e95dcf23faa14f5eb4846c1bb
adiluexe <adiluexe@gmail.com>
2025-03-08 15:06:00 +0800

    Add separate bottom nav bar and placeholder screens

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
+import '../booking/booking_screen.dart';
+import '../workout/workout_screen.dart';
+import '../progress/progress_screen.dart';
+import '../profile/profile_screen.dart';
+import '../../widgets/bottom_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,11 +17,11 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   static final List<Widget> _pages = <Widget>[
-    const PlaceholderScreen(title: 'Home'),
-    const PlaceholderScreen(title: 'Booking'),
-    const PlaceholderScreen(title: 'Workout'),
-    const PlaceholderScreen(title: 'Progress'),
-    const PlaceholderScreen(title: 'Profile'),
+    const HomeContentScreen(),
+    const BookingScreen(),
+    const WorkoutScreen(),
+    const ProgressScreen(),
+    const ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -29,37 +34,23 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(IconlyBold.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(IconlyBold.calendar), label: 'Booking'),
-          BottomNavigationBarItem(icon: Icon(IconlyBold.work), label: 'Workout'),
-          BottomNavigationBarItem(icon: Icon(IconlyBold.chart), label: 'Progress'),
-          BottomNavigationBarItem(icon: Icon(IconlyBold.profile), label: 'Profile'),
-        ],
+      bottomNavigationBar: BottomNavBar(
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
       ),
     );
   }
 }
 
-class PlaceholderScreen extends StatelessWidget {
-  final String title;
-
-  const PlaceholderScreen({super.key, required this.title});
+class HomeContentScreen extends StatelessWidget {
+  const HomeContentScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Text(
-          '$title Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+    return const Center(
+      child: Text(
+        'Home Screen',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
     );
   }
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(IconlyBold.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(IconlyBold.calendar), label: 'Booking'),
+        BottomNavigationBarItem(icon: Icon(IconlyBold.work), label: 'Workout'),
+        BottomNavigationBarItem(icon: Icon(IconlyBold.chart), label: 'Progress'),
+        BottomNavigationBarItem(icon: Icon(IconlyBold.profile), label: 'Profile'),
+      ],
+      currentIndex: currentIndex,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.grey,
+      onTap: onTap,
+    );
+  }
+}
